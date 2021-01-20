6f53bd5dca58ae223c2ca95fafefc09e224ae8d7
zerobits01 <zerobits0101@gmail.com>
2021-01-20 12:06:03 +0330

    adding run server bash script

---
+++ run-server.sh
@@ -0,0 +1,11 @@
+source ~/django-env/bin/activate
+
+cd /vagrant
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000
